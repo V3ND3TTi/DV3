94acbdc0371bea02214b75836ec174578671e455
Devon Vendetti <v3nd3tti@icloud.com>
2022-04-10 01:44:37 -0700

    2nd commit. responsive navbar & home page finished.

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    // SHOW SCROLL TO TOP BUTTON
+    $(window).scroll(function() {
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass('show');
+        } else {
+            $('.scroll-up-btn').removeClass('show');
+        }
+    });
+
+    // SCROLL TO TOP
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({scrollTop: 0});
+    });
+
+    // TOGGLE MENU - NAVBAR
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
+    });
+    
+    // TYPING ANIMATION SCRIPT
+    let typed = new Typed('.typed', {
+        strings: ["Web Developer", "GoLang Enthusiast", "Gamer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
