8fb3914e308d96978e63a944de749bf948018cd9
Devon Vendetti <v3nd3tti@icloud.com>
2022-10-18 07:02:20 -0700

    minor update

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-$(document).ready(function() {
-    // SHOW SCROLL TO TOP BUTTON
-    $(window).scroll(function() {
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass('show');
-        } else {
-            $('.scroll-up-btn').removeClass('show');
-        }
-    });
+$(document).ready(function () {
+  // SHOW SCROLL TO TOP BUTTON
+  $(window).scroll(function () {
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass('show');
+    } else {
+      $('.scroll-up-btn').removeClass('show');
+    }
+  });
 
-    // SCROLL TO TOP
-    $('.scroll-up-btn').click(function() {
-        $('html').animate({scrollTop: 0});
-    });
+  // SCROLL TO TOP
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
+  });
 
-    // TOGGLE MENU - NAVBAR
-    $('.menu-btn').click(function() {
-        $('.navbar .menu').toggleClass('active');
-        $('.menu-btn i').toggleClass('active');
-    });
-    
-    // TYPING ANIMATION SCRIPT
-    let typed = new Typed('.typed', {
-        strings: ["Gamer", "GoLang", "Python"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  // TOGGLE MENU - NAVBAR
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
+  });
+
+  // TYPING ANIMATION SCRIPT
+  let typed = new Typed('.typed', {
+    strings: ['JavaScript', 'TypeScript', 'GoLang', 'WebDev'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 });
